c79250edf03cdb232a3d55c16cf92b733b25e3bb
伊藤翼 <tsubasa8770@gmail.com>
2019-06-21 17:45:41 +0900

    make addresses

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Addresses from './views/Addresses.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -12,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/addresses',
+      name: 'addresses',
+      component: Addresses
     },
     {
       path: '/about',
