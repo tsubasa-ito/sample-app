6aef6b0898f75342ed343bec1723c09446c715c9
伊藤翼 <tsubasa8770@gmail.com>
2019-06-22 00:07:20 +0900

    install firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+const firebaseConfig = {
+    apiKey: "AIzaSyDu8ux4w_hPl0DYmoEjDL5RDhfHOJ7diEw",
+    authDomain: "my-address-pj-1806f.firebaseapp.com",
+    databaseURL: "https://my-address-pj-1806f.firebaseio.com",
+    projectId: "my-address-pj-1806f",
+    storageBucket: "",
+    messagingSenderId: "189060973197",
+    appId: "1:189060973197:web:2678a46ee2c200e5"
+  };
+  firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
